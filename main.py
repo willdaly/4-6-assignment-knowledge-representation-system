00e8c9748b569d6f312e1d45b5e4fcbdb6e1df10
Will <rwilliamdaly@gmail.com>
2026-03-10 18:14:02 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+4.6 Assignment: Knowledge Representation System
+
+Module 4&nbsp;4.6 Assignment: Knowledge Representation System
+
+
+Overview
+You will design, implement, and evaluate a knowledge representation system for a domain of your choosing. This assignment integ
+"""
+
+
+def main():
+    """Main function - implement your solution here."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
