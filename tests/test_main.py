2659785ecde48392c5e591113476191dbf02eb29
Will <rwilliamdaly@gmail.com>
2026-03-10 18:14:03 -0400

    Add tests/test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""
+Tests for 4.6 Assignment: Knowledge Representation System
+"""
+import pytest
+from main import main
+
+
+def test_main():
+    """Test the main function."""
+    # TODO: Add your test cases here
+    pass
